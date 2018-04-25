3d7a1f15104ac0e05432f8930d7f113df9549223
Kay McKelly <frecklestars@gmail.com>
2018-04-25 10:46:12 -0700

    Conference program editor and index pages completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,20 @@
+// TODO later: find a less trash/laggy way to do this sort of thing
 // adapted from https://bootstrapcreative.com/pattern/image-lightbox-modal/
 
-$(document).ready(function() {
-  var $imageSrc;
+// $(document).ready(function() {
+//   var $imageSrc;
   // get source of what you're clicking on and assign it to the variable $imageSrc
-  $('.gallery img').click(function() {
-      $imageSrc = $(this).data('embiggen');
-  });
+  // $('.gallery img').click(function() {
+  //     $imageSrc = $(this).data('embiggen');
+  // });
 
   // when modal is triggered, gt the image within and change its' source to be the one you captured above
-  $('#exampleModalCenter').on('shown.bs.modal', function (e) {
-    $("#image").attr('src', $imageSrc);
-  });
+  // $('#exampleModalCenter').on('shown.bs.modal', function (e) {
+  //   $("#image").attr('src', $imageSrc);
+  // });
 
   // reset on closure (yes, there's more elegant ways to do this but hey, it works)
-  $('#exampleModalCenter').on('hide.bs.modal', function (e) {
-      $("#image").attr('src','#');
-  });
-});
+//   $('#exampleModalCenter').on('hide.bs.modal', function (e) {
+//       $("#image").attr('src','#');
+//   });
+// });
